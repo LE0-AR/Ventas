005a3cdb62fa830e955a33767275292828ec5133
LE0-AR <leonardoacrn12@gmail.com>
2025-02-13 09:20:47 -0600

    sistemade inventario

---
+++ config/app.php
@@ -0,0 +1,27 @@
+<?php
+
+    const APP_URL="http://localhost/VENTAS/";
+    const APP_NAME="VENTAS";
+    const APP_SESSION_NAME="POS";
+
+    /*----------  Tipos de documentos  ----------*/
+    const DOCUMENTOS_USUARIOS=["DPI","NIT","Licencia","Pasaporte","Otro"];
+
+
+    /*----------  Tipos de unidades de productos  ----------*/
+    const PRODUCTO_UNIDAD=["Unidad","Libra","Kilogramo","Caja","Paquete","Lata","Galon","Botella","Tira","Sobre","Bolsa","Saco","Tarjeta","Otro"];
+
+    /*----------  Configuración de moneda  ----------*/
+    const MONEDA_SIMBOLO="Q";
+    const MONEDA_NOMBRE="Quetzal";
+    
+    const MONEDA_DECIMALES="2";
+    const MONEDA_SEPARADOR_MILLAR=",";
+    const MONEDA_SEPARADOR_DECIMAL=".";
+
+
+    /*----------  Marcador de campos obligatorios (Font Awesome) ----------*/
+    const CAMPO_OBLIGATORIO='&nbsp; <i class="fas fa-edit"></i> &nbsp;';
+
+    /*----------  Zona horaria  ----------*/
+    date_default_timezone_set("America/Guatemala");
